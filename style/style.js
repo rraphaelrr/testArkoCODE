0c1d91eec633d9f6c0c26b1dd5475f89f14e8be5
Raphael Souza Rodrigues <raphael.rodrigues@rede.local>
2023-10-13 15:00:30 -0300

    [feature] function notification in push

---
+++ style/style.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Dimensions } from 'react-native';
+import StyleSheet from './styleSheet';
+
+const deviceWidth = Dimensions.get("window").width / 2 + 32;
+const width = Dimensions.get("window").width / 3.2;
+
+const deviceHeight = Dimensions.get("window").height;
+
+const SLIDER_WIDTH = Dimensions.get('window').width;
+
+const style = {
+
+  styles: StyleSheet.create({
+    container: {
+      width: '100%',
+      height: '100%',
+      
+    },
+
+
+
+
+  }),
+
+}
+
+export default style;
---
+++ style/styleSheet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default {
+    colors: {
+        whiteDefault: "#fff",
+        blackDefault: "#000"
+    }
+}
